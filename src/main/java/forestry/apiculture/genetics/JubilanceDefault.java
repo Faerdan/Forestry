64bf141c7f760e8cd772f96aef7d9da5d0324daf
Mark Aherne <faerdan@gmail.com>
2017-06-22 09:55:32 +0100

    Initial commit of Forestry for 1.7.

---
+++ src/main/java/forestry/apiculture/genetics/JubilanceDefault.java
@@ -0,0 +1,44 @@
+/*******************************************************************************
+ * Copyright (c) 2011-2014 SirSengir.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser Public License v3
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
+ *
+ * Various Contributors including, but not limited to:
+ * SirSengir (original work), CovertJaguar, Player, Binnie, MysteriousAges
+ ******************************************************************************/
+package forestry.apiculture.genetics;
+import java.util.Arrays;
+
+import forestry.api.apiculture.IAlleleBeeSpecies;
+import forestry.api.apiculture.IBeeGenome;
+import forestry.api.apiculture.IBeeHousing;
+import forestry.api.apiculture.IJubilanceProvider;
+import forestry.api.core.EnumHumidity;
+import forestry.api.core.EnumTemperature;
+import net.minecraft.world.biome.BiomeGenBase;
+
+public class JubilanceDefault implements IJubilanceProvider {
+
+	public static final JubilanceDefault instance = new JubilanceDefault();
+
+	protected JubilanceDefault() {
+
+	}
+
+	@Override
+	public boolean isJubilant(IAlleleBeeSpecies species, IBeeGenome genome, IBeeHousing housing) {
+		BiomeGenBase biome = housing.getBiome();
+		if (genome.getPrimary().getRequiredBiomeNamePatterns() != null && genome.getPrimary().doesBiomeMatchRequired(biome))
+		{
+			return false;
+		}
+		
+		EnumTemperature temperature = housing.getTemperature();
+		EnumHumidity humidity = housing.getHumidity();
+
+		return temperature == species.getTemperature() && humidity == species.getHumidity();
+	}
+
+}
